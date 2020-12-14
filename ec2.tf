b5703b0c2e4e10977d42f8f3cd77ddb122f9e2c9
chiranjeevpanjwani <66224358+chiranjeevpanjwani@users.noreply.github.com>
2020-12-15 02:37:57 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region     = "us-west-2"
+  region     = "us-east-1"
   access_key = "AKIAXW2ULEXM46JNXFE6"
   secret_key = "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"
 }
 
 resource "aws_instance" "myec2" {
-   ami = "ami-0b0154d3d8011b0cd"
+   ami = "ami-04d29b6f966df1537"
    instance_type = "t2.micro"
 }
