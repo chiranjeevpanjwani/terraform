032416f031ef005c3ea647bf1579479925db50db
chiranjeevpanjwani <66224358+chiranjeevpanjwani@users.noreply.github.com>
2020-12-15 02:21:53 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
-  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+  access_key = "AKIAXW2ULEXM46JNXFE6"
+  secret_key = "g9YcDG5L2euDPMSM5J9FycFBk6gdvbF2NUJMUpqw"
 }
 
 resource "aws_instance" "myec2" {
