ce9082dca927e5ba53b0f50563d9908852fafe2f
chiranjeevpanjwani <66224358+chiranjeevpanjwani@users.noreply.github.com>
2020-12-15 02:50:15 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key = "g9YcDG5L2euDPMSM5J9FycFBk6gdvbF2NUJMUpqw"
 }
 
-resource "aws_instance" "myec2" {
+resource "aws_instance" "newec2" {
    ami = "ami-04d29b6f966df1537"
    instance_type = "t2.micro"
 }
